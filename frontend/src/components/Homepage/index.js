98ac7bef2258592a95d05a5ce87a0dda9c38cbe3
Anabel <villalobos1998@gmail.com>
2022-03-14 11:22:43 -0700

    added gitbuh link and favion

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -10,6 +10,9 @@ export const Homepage = () => {
             <a href="https://www.linkedin.com/in/anabel-villalobos-5772ab196/">
                 <i id="businessIcon" class="fa-solid fa-user-tie"></i>
             </a>
+            <a id="github" href='https://github.com/annvilla1998/campLife'>
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"></img>
+            </a>
             <div id="used">
                 <p>React</p>
                 <p>Redux</p>
